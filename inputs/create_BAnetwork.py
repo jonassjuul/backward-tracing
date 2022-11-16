5eec5564305e606e16337baa813a834073a615d4
Jonas L. Juul <31031606+jonassjuul@users.noreply.github.com>
2022-11-16 14:00:41 +0100

    Delete inputs directory

--- inputs/create_BAnetwork.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-#import sys
-
-
-#RES_FILENAME = sys.argv[1]
-RES_FILENAME = 'BA_network.csv'
-G = nx.barabasi_albert_graph(250000, 2)
-nx.write_edgelist(G, RES_FILENAME,delimiter=',')
