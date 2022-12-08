bf8a77d8e3cb22013f8066e27e62c5d7e55e9914
jonassjuul <jlju@dtu.dk>
2022-12-08 15:22:22 +0100

    Created person-gathering networks

--- inputs/create_Person-Gatheringnetwork.py
+++ inputs/create_Person-Gatheringnetwork.py
@@ -1,136 +1,134 @@
 import os
 import sys
+import random
 
 import networkx as nx
 import numpy as np
 
-import graph_tool.all as gt
-#import seir
+def draw_powerlaw_numbers(exponent,min_value,max_value,number_of_draws):
+    x_range=np.arange(min_value,max_value+1)                    # Possible degrees
+    x_topower = np.power(x_range,exponent)                      # Possible degrees to exponent
+    pdf_unnormalized = 1/x_topower
+    pdf_normalized=pdf_unnormalized/np.sum(pdf_unnormalized)    # Normalized pdf (which is proportional to degrees to exponent)
+    cdf_normalized = np.cumsum(pdf_normalized)                  # Corresponding cdf.
+
+    random_numbers = np.random.random(number_of_draws)
+
+    drawn_degrees = []
+    for number in random_numbers : # For each random number, find out what drawn degree this corresponds to.
+        try_entry = 0
+        found=False
+        while (found == False) :
+            difference = number-cdf_normalized[try_entry]
+            if (difference <= 0) :
+                found = True
+            else :
+                try_entry +=1 
+
+        drawn_degrees.append(min_value + try_entry)
 
 
-def generate_power_law_bipartite_net(
-    N, frac_left_node, gamma, ave_deg, min_deg_left, min_deg_right, node_class
-):
-    """
-    Generate power law bipartite network.
-    Params
-    ------
-    N : int
-        Number of nodes
-    frac_left_node : float
-        Fraction of nodes on the left part.
-    gamma : float
-        Power-law exponent (same expoent for both sides)
-    ave_deg : float
-        Average degree
-    min_deg_left : int
-        Minimum degree for nodes on the left part
-    min_deg_right : int
-        Minimum degree for nodes on the right part
-    node_class : list of str
-        Name of the class for the left and right nodes
-        node_class[0] : str for left nodes.
-        node_class[1] : str for right nodes.
-    Return
-    ------
-    G : networkx.Graph
-    """
+    return drawn_degrees
 
-    def zipf(a, min, max, size=None):
-        """
-        Generate Zipf-like random variables,
-        but in inclusive [min...max] interval
-        """
-        v = np.arange(min, max + 1)  # values to sample
-        p = 1.0 / np.power(v, a)  # probabilities
-        p /= np.sum(p)  # normalized
-        return np.random.choice(v, size=size, replace=True, p=p)
+def align_stub_numbers(people_degrees,gathering_degrees) :
 
-    def add_n_stabs(deg, n):
-        """
-        Add n stabs to degree sequence
-        """
-        stubs = np.random.choice(
-            np.arange(len(deg)), size=int(n), replace=True, p=deg / np.sum(deg)
-        )
-        for s in stubs:
-            deg[s] += 1
-        return deg
+    stub_differences = np.sum(people_degrees)-np.sum(gathering_degrees) # Positive if more stubs for people.
 
-    def to_graphical_deg_seq(deg_left, deg_right):
-        """
-        Make the degree sequence to be graphical
-        by adding edges
-        """
-        deg_left_sum = np.sum(deg_left)
-        deg_right_sum = np.sum(deg_right)
+    if (stub_differences > 0) :
+        add_edges_to = np.random.randint(0,len(gathering_degrees),size=stub_differences)
 
-        if deg_left_sum < deg_right_sum:
-            deg_left = add_n_stabs(deg_left, deg_right_sum - deg_left_sum)
-        elif deg_left_sum > deg_right_sum:
-            deg_right = add_n_stabs(deg_right, deg_left_sum - deg_right_sum)
+        for node in add_edges_to :
+            gathering_degrees[node] += 1
 
-        return deg_left, deg_right
+    elif (stub_differences < 0) :
+        add_edges_to = np.random.randint(0,len(people_degrees),size=-stub_differences)
 
-    # Compute the number of nodes
-    N_left = int(N * frac_left_node)
-    N_right = N - N_left
+        for node in add_edges_to :
+            people_degrees[node] += 1
+    return people_degrees,gathering_degrees
 
-    # Generate degree sequence
-    deg_left = zipf(3, min_deg_left, N_right, size=N_left)
-    deg_right = zipf(3, min_deg_right, N_left, size=N_right)
+def bipartite_configuration_model(degrees_A,degrees_B) :
+    
+    
+    
+    # Create list of stubs
+    stublist_A = []
+    stublist_B = []
 
-    # Rescale such that the average degree is the prescribed average degree
-    E = ave_deg * (N_left + N_right)
-    deg_left = np.clip(np.round(deg_left * E / np.sum(deg_left)), min_deg_left, N_right)
-    deg_right = np.clip(
-        np.round(deg_right * E / np.sum(deg_right)), min_deg_right, N_left
-    )
 
-    # Make them graphical degree sequences
-    deg_left, deg_right = to_graphical_deg_seq(deg_left, deg_right)
+    for nodenumber in range (len(degrees_A)) :
+        for stub in range (degrees_A[nodenumber]) :
+            stublist_A.append(nodenumber) 
 
-    # Prepare parameters for graph-tool
-    E = np.sum(deg_right)
-    gt_params = {
-        "out_degs": np.concatenate([np.zeros_like(deg_left), deg_right]).astype(int),
-        "in_degs": np.concatenate([deg_left, np.zeros_like(deg_right)]).astype(int),
-        "b": np.concatenate([np.zeros(N_left), np.ones(N_right)]),
-        "probs": np.array([[0, 0], [E, 0]]),
-        "directed": True,
-        "micro_degs": True,
-    }
+    offsetnumber = len(degrees_A) # nodes in group B have node IDs starting where node IDs in group A ended.
+    for nodenumber in range (len(degrees_B)) :
+        for stub in range (degrees_B[nodenumber]) :
+            stublist_B.append(nodenumber+offsetnumber )
 
-    # Generate the network until the degree sequence
-    # satisfied the thresholds
-    while True:
-        g = gt.generate_sbm(**gt_params)
-        A = gt.adjacency(g).T
-        A.data = np.ones_like(A.data)
-        outdeg = np.array(A.sum(axis=1)).reshape(-1)[N_left:]
-        indeg = np.array(A.sum(axis=0)).reshape(-1)[:N_left]
-        if (np.min(indeg) >= min_deg_left) and (np.min(outdeg) >= min_deg_right):
-            break
+    # permute stubs.
+    #stublist_A.shuffle()
+    random.shuffle(stublist_B)
 
-    # Convert to the networkx objet
-    G = nx.from_scipy_sparse_matrix(A, create_using=nx.Graph)
+    return stublist_A,stublist_B
 
-    # Add attributes to the nodes
-    node_attr = {i: node_class[int(i > N_left)] for i in range(N)}
-    nx.set_node_attributes(G, node_attr, "class")
-    return G
+def save_edgelist(stublist_A,stublist_B,filename):
 
-N = 250000
-frac_left_node = 0.2
-ave_deg = 5
-min_deg_left = 2
-min_deg_right = 1
-gamma = -3
-node_class = ["gathering","people"]
+    f = open(filename,'w')
+    f.write("Node1,Node2,Brackets")
+    for edgenumber in range (len(stublist_A)) :
+        f.write("\n%i,%i,{}"%(stublist_A[edgenumber],stublist_B[edgenumber]))
+    f.close()
 
-G = generate_power_law_bipartite_net(N, frac_left_node, gamma, ave_deg, min_deg_left, min_deg_right, node_class)
+# Now draw degree sequences
+n_people = 200000
+n_gatherings=85000
 
-print(G.edges())
+gathering_degrees = draw_powerlaw_numbers(exponent=3,
+                            min_value=2,
+                            max_value=n_people,
+                            number_of_draws=n_gatherings) # Gatherings have at least 2 people. 
+
+people_degrees = draw_powerlaw_numbers(exponent=3,
+                            min_value=1,
+                            max_value=n_people,
+                            number_of_draws=n_people) #People have at least degree 1.
+
+print("BEFORE")
+print("--------------")
+print("Sum of gathering degrees",np.sum(gathering_degrees), "Av degree:",np.sum(gathering_degrees)/n_gatherings)
+print("Sum of people degrees",np.sum(people_degrees), "Av degree:",np.sum(people_degrees)/n_people)
+print("Difference between degree sums (people-gath)",np.sum(people_degrees)-np.sum(gathering_degrees))
+
+print("Highest gathering degree",np.max(gathering_degrees))
+print("Highest people degree",np.max(people_degrees))
+
+
+
+# It is very unlikely that there are now equally many stubs for people and gatherings.
+# Add edges to make stubs match each other. (This makes the degree distribution for either gatherings or people. Kojaku et al. did something different but similar)
+people_degrees,gathering_degrees = align_stub_numbers(people_degrees,gathering_degrees)
+
+
+print("AFTER")
+print("--------------")
+print("Sum of gathering degrees",np.sum(gathering_degrees), "Av degree:",np.sum(gathering_degrees)/n_gatherings)
+print("Sum of people degrees",np.sum(people_degrees), "Av degree:",np.sum(people_degrees)/n_people)
+print("Difference between degree sums (people-gath)",np.sum(people_degrees)-np.sum(gathering_degrees))
+
+print("Highest gathering degree",np.max(gathering_degrees))
+print("Highest people degree",np.max(people_degrees))
+
+
+# Now create the configuration model graph
+# Gatherings have labels [0, n_gathering[
+gathering_stubs,people_stubs=bipartite_configuration_model(gathering_degrees,people_degrees)
+
+# save to file.
+filename = 'person_gathering_network.csv'
+save_edgelist(gathering_stubs,people_stubs,filename)
+
+
+
 
 
 
